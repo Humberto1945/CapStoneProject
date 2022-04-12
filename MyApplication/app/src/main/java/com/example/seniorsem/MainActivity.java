024cd9742c25425e0b7a112c79147e317c0cb5c7
Humberto <titigarcia519@gmail.com>
2022-04-12 09:35:40 -0400

    Finishing the image segmentation view

--- MyApplication/app/src/main/java/com/example/seniorsem/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/seniorsem/MainActivity.java
@@ -1,30 +1,33 @@
 package com.example.seniorsem;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import org.pytorch.IValue;
 import org.pytorch.Module;
 import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
-
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Uri selectedImage;
     Bitmap imageBitmap;
+    Module preTrainedModel;
     // Module moduleResNet;
     int imageSize = 224;
     // class segmentation corresponding indices
@@ -33,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
     int aeroplane = 1;
     int bicycle = 2;
     int bird = 3;
-    int boat = 5;
+    int boat = 4;
+    int bottle =5;
     int bus = 6;
     int car =7;
     int cat = 8;
@@ -48,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
     int sheep = 17;
     int sofa = 18;
     int train = 19;
-    int monitor = 20;
+    int tvmonitor = 20;
+    int CLASSNUM = 21;
 
 
     @Override
@@ -58,8 +63,12 @@ public class MainActivity extends AppCompatActivity {
         // Setting buttons
         Button loadImage = findViewById(R.id.loadButton);
         Button segmentImage = findViewById(R.id.segmentButton);
-
-
+        // Import the pretrained-model
+        try {
+            preTrainedModel= Module.load(readingFilePath(this,"test_model.ptl"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Lets the user select an image from their camera roll
         loadImage.setOnClickListener(new View.OnClickListener() {
@@ -74,32 +83,90 @@ public class MainActivity extends AppCompatActivity {
         segmentImage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                //getting the inputs from the image
                 final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(imageBitmap,
-                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
-                        TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-                // Getting the input values
+                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
                 final float[] inputs = inputTensor.getDataAsFloatArray();
-                // TODO extract data from the inputs based on the model
+                //setting the values to a map to get the class num
+                Map<String, IValue> outTensors = preTrainedModel.forward(IValue.from(inputTensor)).toDictStringKey();
+                // VOC key word out to tensor
+                final Tensor outputTensor = outTensors.get("out").toTensor();
+                final float[] scores = outputTensor.getDataAsFloatArray();
+
                 int width = imageBitmap.getWidth();
                 int height = imageBitmap.getHeight();
 
-
                 int[] intValues = new int[width * height];
-                // for loop to go through outputted size
+                //looping through to get the class
+                for (int j = 0; j < height; j++) {
+                    for (int k = 0; k < width; k++) {
+                        int maxi = 0, maxj = 0, maxk = 0;
+                        double maxnum = -Double.MAX_VALUE;
 
-                for(int i = 0; i< width; i++){
-                    for(int j = 0; j< height; j++ ){
-
+                        for (int i = 0; i < CLASSNUM; i++) {
+                            float score = scores[i * (width * height) + j * width + k];
+                            if (score > maxnum) {
+                                maxnum = score;
+                                maxi = i; maxj = j; maxk = k;
+                            }
+                        }
+                        if (maxi == aeroplane)
+                            intValues[maxj * width + maxk] = Color.rgb(128,0,0);
+                        else if (maxi == bicycle)
+                            intValues[maxj * width + maxk] = Color.rgb(0,128,0);
+                        else if (maxi == bird)
+                            intValues[maxj * width + maxk] = Color.rgb(128,128,0);
+                        else if (maxi == boat)
+                            intValues[maxj * width + maxk] = Color.rgb(0,0,128);
+                        else if (maxi == bottle)
+                            intValues[maxj * width + maxk] = Color.rgb(128,0,128);
+                        else if (maxi == bus)
+                            intValues[maxj * width + maxk] = Color.rgb(0,128,128);
+                        else if (maxi == car)
+                            intValues[maxj * width + maxk] = Color.rgb(128,128,128);
+                        else if (maxi == cat)
+                            intValues[maxj * width + maxk] = Color.rgb(64,0,0);
+                        else if (maxi == chair)
+                            intValues[maxj * width + maxk] = Color.rgb(192,0,0);
+                        else if (maxi == cow)
+                            intValues[maxj * width + maxk] = Color.rgb(64,128,0);
+                        else if (maxi == diningtable)
+                            intValues[maxj * width + maxk] = Color.rgb(192,128,0);
+                        else if (maxi == dog)
+                            intValues[maxj * width + maxk] = Color.rgb(64,0,128);
+                        else if (maxi == horse)
+                            intValues[maxj * width + maxk] = Color.rgb(192,0,128);
+                        else if (maxi == mortorbike)
+                            intValues[maxj * width + maxk] = Color.rgb(64,128,128);
+                        else if (maxi == person)
+                            intValues[maxj * width + maxk] = Color.rgb(192,128,128);
+                        else if (maxi == pottedPlant)
+                            intValues[maxj * width + maxk] = Color.rgb(0,64,0);
+                        else if (maxi == sheep)
+                            intValues[maxj * width + maxk] = Color.rgb(128,64,0);
+                        else if (maxi == sofa)
+                            intValues[maxj * width + maxk] = Color.rgb(0,192,0);
+                        else if (maxi == train)
+                            intValues[maxj * width + maxk] = Color.rgb(128,192,0);
+                        else if (maxi ==tvmonitor )
+                            intValues[maxj * width + maxk] = Color.rgb(0,64,128);
+                        else if (maxi ==tvmonitor )
+                            intValues[maxj * width + maxk] = Color.rgb(0,64,128);
+                        else if (maxi ==unlabelled )
+                            intValues[maxj * width + maxk] = Color.rgb(224,224,192);
+                        else
+                            intValues[maxj * width + maxk] = Color.rgb(0,0,0);;
                     }
                 }
-                // Displaying the segmented images
-                ImageView imageView = findViewById(R.id.imageView);
-                Bitmap segmentedBitMap = Bitmap.createScaledBitmap(imageBitmap, width, height, true);
-                // Copying the segmented bit map into an output to translate into pixels
-                Bitmap outputBitmap = segmentedBitMap.copy(segmentedBitMap.getConfig(), true);
-                outputBitmap.setPixels(intValues, 0, outputBitmap.getWidth(), 0, 0, outputBitmap.getWidth(), outputBitmap.getHeight());
-                imageView.setImageBitmap(outputBitmap);
 
+                //rescaling the image for better input
+                Bitmap bitmapScaled = Bitmap.createScaledBitmap(imageBitmap, width, height, true);
+
+                Bitmap outputBitmap = bitmapScaled.copy(bitmapScaled.getConfig(), true);
+                outputBitmap.setPixels(intValues, 0, outputBitmap.getWidth(), 0, 0, outputBitmap.getWidth(), outputBitmap.getHeight());
+                //sets the segmented image to the imageview
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(outputBitmap);
             }
         });
 
@@ -124,11 +191,26 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    //getting the path for the model
+    public static String readingFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+            if (file.exists() && file.length() > 0) {
+                return file.getAbsolutePath();
+            }
 
-    /*public void getSegmentation(View view){
-
-    }*/
-
-
-
+        try (InputStream is = context.getAssets().open(assetName)) {
+         try (OutputStream os = new FileOutputStream(file)) {
+            byte[] buffer = new byte[4 * 1024];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+        }
+        return file.getAbsolutePath();
+    }
 }
+
+
+
+}//end of activity
