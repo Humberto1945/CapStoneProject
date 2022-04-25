cb89949a838f8c06e8eb4639fe562e8732a3636e
nmcrumpton <89715917+nmcrumpton@users.noreply.github.com>
2022-04-25 11:19:46 -0400

    Update MeanSDCalc.py

--- Python/MeanSDCalc.py
+++ Python/MeanSDCalc.py
@@ -4,86 +4,75 @@ import numpy as np
 import torch
 import torchvision
 from torchvision import transforms
+import math
 
+mean0 = 0.
 mean1 = 0.
 mean2 = 0.
-mean3 = 0.
+std0 = 0.
 std1 = 0.
 std2 = 0.
-std3 = 0.
 
-files = 0
-# pixCount = 0
+pixCount = 0
 
 """ 
-    The JPEGImages folder contains 17125 images, but we do not use that many.
-    For the time being the mean is calculated based on all of them
+    Calculation of data normalization values based on the training set
 """
 
-folder_path = '/content/drive/MyDrive/Python/PascalVOC2012/VOC2012/JPEGImages'
+folder_path = '/content/drive/MyDrive/Python/PascalVOC2012/VOC2012/JPEGImages/'
+train_set_list = '/content/drive/MyDrive/Python/samples/train_set.txt'
+train_set = []
 
-# file_count = sum(len(files) for _, _, files in os.walk(folder_path))
-# file_count = len(os.listdir(folder_path))
-# print(file_count)
+fi = open(train_set_list)
+for line in iter(fi):
+  line = line.strip()
+  train_set.append(line)
+fi.close()
 
-for filename in os.listdir(folder_path):
+for filename in train_set:
   # print(filename)
-  im = Image.open(os.path.join(folder_path, filename))
+  im = np.array(Image.open(os.path.join(folder_path, filename)))
 
-  convertTensor = transforms.ToTensor()
-  # convertSize = transforms.CenterCrop(512)
+  mean0 += np.sum(im[:,:,0])
+  mean1 += np.sum(im[:,:,1])
+  mean2 += np.sum(im[:,:,2])
+  # print(mean1)
 
-  # adding the size conversion greatly decreases the mean due to added empty pixels
+  # print(im.shape[0])
+  # print(im.shape[1])
+  pixCount += (im.shape[0] * im.shape[1])
 
-  # im = convertSize(im)
-  imTensor = convertTensor(im)
+  # print("M0: " + str(mean0))
+  # print("M1: " + str(mean1))
+  # print("M2: " + str(mean2))
 
-  files += 1
+# print(pixCount)
+# print(pixCount * 255)
 
-  # print(mean)
+mean0 /= (pixCount * 255)
+mean1 /= (pixCount * 255)
+mean2 /= (pixCount * 255)
 
-  # print(imTensor)
-
-  # print(imTensor[:,:,0])
-  # print(imTensor[:,:,1])
-  # print(imTensor[:,:,2])
-  
-  # print(imTensor.shape[1])
-  # print(imTensor.shape[2])
-  
-  mean1 += torch.mean(imTensor[:,:,0])
-  mean2 += torch.mean(imTensor[:,:,1])
-  mean3 += torch.mean(imTensor[:,:,2])
-
-  std1 += torch.std(imTensor[:,:,0])
-  std2 += torch.std(imTensor[:,:,1])
-  std3 += torch.std(imTensor[:,:,2])
-
-  # pixCount += imTensor.shape[1] * imTensor.shape[2]
-  # print(pixCount)
-
-  # print("M1: " + str(mean1.item()))
-  # print("M2: " + str(mean2.item()))
-  # print("M3: " + str(mean3.item()))
-
-  # print("SD1: " + str(std1.item()))
-  # print("SD2: " + str(std2.item()))
-  # print("SD3: " + str(std3.item()))
-
-mean1 /= files
-mean2 /= files
-mean3 /= files
-
-std1 /= files
-std2 /= files
-std3 /= files
-
-print("Final M1: " + str(round(mean1.item(), 3)))
-print("Final M2: " + str(round(mean2.item(), 3)))
-print("Final M3: " + str(round(mean3.item(), 3)))
+print("Final M0: " + str(round(mean0, 3)))
+print("Final M1: " + str(round(mean1, 3)))
+print("Final M2: " + str(round(mean2, 3)))
 
 print("\n")
 
-print("Final SD1: " + str(round(std1.item(), 3)))
-print("Final SD2: " + str(round(std2.item(), 3)))
-print("Final SD3: " + str(round(std3.item(), 3)))
+for filename in train_set:
+  im = np.array(Image.open(os.path.join(folder_path, filename)))
+
+  for row in im:
+    for pix in row:
+      std0 += (((pix[0] / 255) - mean0) ** 2)
+      std1 += (((pix[1] / 255) - mean1) ** 2)
+      std2 += (((pix[2] / 255) - mean2) ** 2)
+
+std0 = math.sqrt(std0 / pixCount)
+std1 = math.sqrt(std1 / pixCount)
+std2 = math.sqrt(std2 / pixCount)
+
+
+print("Final SD0: " + str(round(std0, 3)))
+print("Final SD1: " + str(round(std1, 3)))
+print("Final SD2: " + str(round(std2, 3)))
